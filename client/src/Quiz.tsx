e23060c94e58d8525ff0ed256afd7960451866b3
clement <clementadebola54@gmail.com>
2024-08-08 12:38:13 +0100

    changes

--- client/src/Quiz.tsx
+++ client/src/Quiz.tsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import { useSpring, animated, config } from "react-spring";
 import {
   FiClock,
-  FiSettings,
   FiThumbsUp,
   FiThumbsDown,
   FiChevronDown,
